50ba3f01015f4f36138bcc8550ffab981ca033c6
kiannadalton <kiannadalton@aol.com>
2024-06-08 23:55:18 -0400

    Completed assignment

--- PuppyPals/src/App.jsx
+++ PuppyPals/src/App.jsx
@@ -1,17 +1,44 @@
 import {useState} from 'react'
 import {puppyList} from './data.js'
 import './App.css'
+import './path-to-css.css'
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList);
-
-  console.log(puppies);
+  const [featPupId, setFeatPupId] = useState(null)
   
+  console.log(puppies);
+
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId);
+  console.log(featuredPup);
+
   return (
     <div className='App'>
+
+    <h1>Puppy Pals</h1>
+    
       { puppies.map((puppy) => {
-     return <p key={puppy.id}>{puppy.name}</p>;
+
+        // const featuredPup = puppies.find((pup)=> pup.id === featPupId)
+        // console.log(featuredPup)
+
+        return (<p onClick={() => {setFeatPupId(puppy.id)}} key={puppy.id}>
+          {puppy.name}
+        </p>
+
+     );
    })}
+      <h1>Puppy ID:</h1>
+      { featPupId && (
+         <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+         </div>  
+        )}
+    
     </div>
   )
 }
